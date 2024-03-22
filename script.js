64fbd9d664e3b4235e21d54ca6376a57cd2181a6
DaisyKinyua <daisywanjiku132@gmail.com>
2024-03-22 14:56:35 +0300

    manipulated html using js

---
+++ script.js
@@ -0,0 +1,36 @@
+// PROJECT Description:
+/*
+* You are tasked with manipulating a simple webpage using JavaScript.
+* The webpage contains a paragraph (<p>) element with the id paragraph and an empty div element with the id imageContainer.
+* Your goal is to select the paragraph element, change its content to "Hello World", and then create an image element (<img>) with the source attribute set to "image.jpg" and append it to the imageContainer div.
+
+// INSTRUCTIONS
+
+/* Task 1:
+ *Select the paragraph element with the id paragraph and change its content to "Hello World".
+
+/* Task 2:
+* Using JavaScript, create an image element with the source attribute set to "image.jpg".
+
+/* Task 3:
+* Append the created image element to the div element with the id imageContainer.
+
+
+*/
+
+// Write your code here. SUCCESS
+let p = document.getElementById("paragraph");
+p.innerHTML="Hello world";
+
+
+let imageContainer = document.getElementById("imageContainer");
+let image = document.createElement("img");
+image.src = "image.jpg";
+imageContainer.appendChild(image);
+
+
+
+
+let footer = document.querySelector("footer");
+let paragraph = footer.querySelector("p");
+paragraph.innerHTML = "Daisy";
